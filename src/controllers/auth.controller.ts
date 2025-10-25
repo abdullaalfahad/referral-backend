a8aaf7d6ec6ee8fd253d8c1e573ec8b9892f1481
abdullaalfahad <aafahad02@gmail.com>
2025-10-26 02:17:43 +0600

    fix(login): error message if user not found

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -122,7 +122,7 @@ export const login = async (req: Request, res: Response) => {
 
     const user = await User.findOne({ email });
     if (!user)
-      return res.status(401).json({ message: "Invalid email or password" });
+      return res.status(404).json({ message: "User not found!" });
 
     const isMatch = await bcrypt.compare(password, user.passwordHash);
     if (!isMatch)
